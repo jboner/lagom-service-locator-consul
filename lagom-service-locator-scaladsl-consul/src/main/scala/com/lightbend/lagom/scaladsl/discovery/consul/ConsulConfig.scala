3fc70bdb4e5e087aadeb3c57fe304ec3bd2be3c9
Bruno Unna <bunna@getvisibility.com>
2018-12-12 10:44:17 +0000

    Use the newer typed getters from configuration

--- lagom-service-locator-scaladsl-consul/src/main/scala/com/lightbend/lagom/scaladsl/discovery/consul/ConsulConfig.scala
+++ lagom-service-locator-scaladsl-consul/src/main/scala/com/lightbend/lagom/scaladsl/discovery/consul/ConsulConfig.scala
@@ -5,18 +5,23 @@ import play.api.Configuration
 
 trait ConsulConfig {
   def agentHostname: String
+
   def agentPort: Int
+
   def scheme: String
+
   def routingPolicy: RoutingPolicy
 }
 
 object ConsulConfig {
-  class ConsulConfigImpl  (config: Configuration) extends ConsulConfig {
-    override val agentHostname = config.getString("lagom.discovery.consul.agent-hostname").get
-    override val agentPort = config.getInt("lagom.discovery.consul.agent-port").get
-    override val scheme = config.getString("lagom.discovery.consul.uri-scheme").get
-    override val routingPolicy = RoutingPolicy(config.getString("lagom.discovery.consul.routing-policy").get)
+
+  class ConsulConfigImpl(config: Configuration) extends ConsulConfig {
+    override val agentHostname: String = config.get[String]("lagom.discovery.consul.agent-hostname")
+    override val agentPort: Int = config.get[Int]("lagom.discovery.consul.agent-port")
+    override val scheme: String = config.get[String]("lagom.discovery.consul.uri-scheme")
+    override val routingPolicy: RoutingPolicy = RoutingPolicy(config.get[String]("lagom.discovery.consul.routing-policy"))
   }
+
 }
 
 object RoutingPolicy {
@@ -27,7 +32,11 @@ object RoutingPolicy {
     case unknown => throw new BadValue("lagom.discovery.consul.routing-policy", s"[$unknown] is not a valid routing algorithm")
   }
 }
+
 sealed trait RoutingPolicy
+
 case object First extends RoutingPolicy
+
 case object Random extends RoutingPolicy
+
 case object RoundRobin extends RoutingPolicy
